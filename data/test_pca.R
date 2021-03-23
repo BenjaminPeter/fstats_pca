f4ddf6443e02f6fb1e67538c517506eb6fc0324d
benjamin_peter <benjamin_peter@eva.mpg.de>
2021-03-23 16:24:22 +0100

    setup admixtools to do pca-like stuff

---
+++ data/test_pca.R
@@ -0,0 +1,19 @@
+require(admixtools)
+require(tidyverse)
+require(glue)
+
+
+MISSING = c("Irish_Ulster", "Polish", "German", "Irish", "Shetlandic", "Sorb")
+
+list1 = read_table("WestEurasian_PCA.list", col_names=F)[,1] %>% unlist %>% 
+    unname
+sample_list = list1[!list1 %in% MISSING]
+sample_list_og = c(sample_list, "Mbuti")
+
+data_loc = '/mnt/ancient/ModernHuman/ReichLab/reich_public_geno_v44.3/v44.3_HO_public'
+inds = read_table2(glue("{data_loc}.ind"), col_names=c("iid","sex", "pop"))
+
+f2loc1 = 'pca_test1'
+
+f2s = admixtools::extract_f2(data_loc, pops=sample_list_og, outdir=f2loc1, maxmem=64000, blgsize=.5)
+
