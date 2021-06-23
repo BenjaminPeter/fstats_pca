455a5908122118f0a7edd4b295a364bf09aac4a1
benjamin_peter <benjamin_peter@eva.mpg.de>
2021-06-23 21:00:34 +0200

    *tex

--- data/prep_data_pca.R
+++ data/prep_data_pca.R
@@ -10,20 +10,20 @@ list1 = read_table("WestEurasian_PCA.list", col_names=F)[,1] %>% unlist %>%
 sample_list = list1[!list1 %in% MISSING]
 sample_list_og = c(sample_list, "Mbuti")
 
+sample_list %>% as.data.frame %>% write_csv("subdata/westeurasian1.poplist", col_names=F)
+
 data_loc = '/mnt/ancient/ModernHuman/ReichLab/reich_public_geno_v44.3/v44.3_HO_public'
 inds = read_table2(glue("{data_loc}.ind"), col_names=c("iid","sex", "pop"))
 
-#data_loc = '/mnt/ancient/ModernHuman/ReichLab/reich_public_geno_v44.3/v44.3_1240K_public'
-#inds = read_table2(glue("{data_loc}.ind"), col_names=c("iid","sex", "pop"))
 
 
-f2loc1 = 'pca_test1'
-f2loc1 = 'pca_test1c'
+f2loc1 = 'westeurasian1'
+#f2loc1 = 'pca_test1c'
 
-afs = admixtools::extract_afs(data_loc, pops=sample_list_og, 
-                              outdir=f2loc1,format="eigenstrat", 
+afs = admixtools::extract_afs(data_loc, pops=sample_list, 
+                              outdir=glue("{f2loc1}_afs"),format="eigenstrat", 
                               cols_per_chunk=1000)
-#f2s = admixtools::extract_f2(data_loc, pops=sample_list_og, 
-#                             outdir=f2loc1, maxmem=64000, blgsize=.5,
-                            # format="eigenstrat")
+#f2s = admixtools::extract_f2(data_loc, pops=sample_list, 
+#                             outdir=f2loc1, maxmem=64000, blgsize=.05,
+#                             overwrite=T)
 
